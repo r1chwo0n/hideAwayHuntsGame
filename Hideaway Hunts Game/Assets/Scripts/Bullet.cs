0b2cdd2613aee557381b0c6f4933e56da0c8a574
r1chwo0n <woonzy09@gmail.com>
2025-05-21 08:38:16 +0700

    about gun controller

---
+++ Hideaway Hunts Game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifeTime = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // อาจทำลายศัตรู
+        Destroy(gameObject);
+    }
+}
---
+++ Hideaway Hunts Game/Assets/Scripts/Controllers/GunController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce = 20f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) // คลิกซ้าย / Ctrl / ปุ่ม Fire
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+    }
+}
